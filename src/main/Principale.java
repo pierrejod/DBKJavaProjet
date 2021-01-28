38c167a1902f1efa8374ac263195505e19714eaf
delattre <pierrejo74@gmail.com>
2021-01-28 20:50:51 +0100

    Ajout du main

--- src/main/Principale.java
+++ src/main/Principale.java
@@ -1,10 +1,108 @@
 package main;
 
-public class Principale {
+import java.awt.Color;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import Tableau.Tableau;
 
-	}
+//Class main qui va permettre d'avoir la page principale et l'accès à la deuxième page fenetreinventaire
+public class Principale extends javax.swing.JFrame {
+
+
+    public Principale() {
+        initComponents();
+        this.setTitle("Gestion de fruits et légumes");
+        this.setLocationRelativeTo(null);
+        setSize(700,500);
+        
+    }
+
+
+
+  
+    private void initComponents() {
+
+        desktopPane = new javax.swing.JDesktopPane();
+        menuBar = new javax.swing.JMenuBar();
+        fileMenu = new javax.swing.JMenu();
+        openMenuItem = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        fileMenu.setMnemonic('f');
+        fileMenu.setText("Gestion");
+
+        openMenuItem.setMnemonic('o');
+        openMenuItem.setText("Inventaire");
+        openMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                openMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(openMenuItem);
+
+
+        
+
+        menuBar.add(fileMenu);
+
+        setJMenuBar(menuBar);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(desktopPane, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(desktopPane, javax.swing.GroupLayout.DEFAULT_SIZE, 279, Short.MAX_VALUE)
+        );
+
+        pack();
+    }
+
+
+    private void openMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
+         
+        FenetreSaisie fi = new FenetreSaisie();
+        desktopPane.add(fi);
+        fi.setVisible(true);
+    }
+
+
+    public static void main(String args[]) {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Principale.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Principale.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Principale.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Principale.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Principale().setVisible(true);
+            }
+        });
+    }
+
+
+    private javax.swing.JDesktopPane desktopPane;
+    private javax.swing.JMenu fileMenu;
+    private javax.swing.JMenuBar menuBar;
+    private javax.swing.JMenuItem openMenuItem;
+
 
 }
