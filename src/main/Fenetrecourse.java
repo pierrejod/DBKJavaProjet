b797dde73a4a1cfc8364658108ecb5d4d8e75568
delattre <pierrejo74@gmail.com>
2021-02-04 18:13:35 +0100

    commit deuxieme fenetre

--- src/main/Fenetrecourse.java
+++ src/main/Fenetrecourse.java
@@ -18,11 +18,21 @@ public class Fenetrecourse extends javax.swing.JInternalFrame {
 	      //model = (DefaultTableModel) table_inv.getModel();
 	  }
 	  private void initComponents() {
+		  
+	      jPanel2 = new javax.swing.JPanel();
+		  
 		  setSize(200,100);
 	      setVisible(true);
 	      setClosable(true);
 	      setIconifiable(true);
 	      setMaximizable(true);
 	      setResizable(false);
+	      
+	      jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+	      jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Inventaire"));
+	      
+	      javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+	      jPanel2.setLayout(jPanel2Layout);
 	  }
+	  private javax.swing.JPanel jPanel2;
 }
